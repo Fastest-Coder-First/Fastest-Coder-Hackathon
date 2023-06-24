825479c8785b794639864e8c6666f059ddc59229
Santosh Sharma <santoshs2@hcl.com>
2023-06-24 17:01:31 +0530

    Added 2 additional test cases

--- TestWeatherReport.py
+++ TestWeatherReport.py
@@ -72,6 +72,28 @@ class TestWeatherReport(unittest.TestCase):
         # check whether weather report is none
         self.assertIsNotNone(weather_report)
 
+    # test create formatted report method with invalid city name
+    def test_create_formatted_report_with_invalid_city_name(self):
+        # initialize weather report class
+        weather_report = WeatherReport()
+
+        # get weather report for the given city name
+        weather_report = weather_report.create_formatted_report("invalid_city_name")
+
+        # check whether weather report is none
+        self.assertIsNone(weather_report)
+
+    # test create formatted report method with valid city name
+    def test_create_formatted_report_with_valid_city_name(self):
+        # initialize weather report class
+        weather_report = WeatherReport()
+
+        # get weather report for the given city name
+        weather_report = weather_report.create_formatted_report("London")
+
+        # check whether weather report is none
+        self.assertIsNotNone(weather_report)
+
 # main block
 if __name__ == "__main__":
     unittest.main()
